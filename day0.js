49ebe791658bf463e69a3229ad2b8341b312ae47
codrelphi <chancerel.codjovi@gmail.com>
2021-12-24 10:02:07 +0100

    Day0: printing a string on stdout

---
+++ day0.js
@@ -0,0 +1,12 @@
+/*
+	date: 24.12.2021
+	auteur: Chancerel Codjovi (aka codrelphi)
+	source: https://www.hackerrank.com/challenges/js10-hello-world/problem
+*/
+
+
+function greeting(parameterVariable) {
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+    
+}
