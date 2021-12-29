46e7bdf6c746922a4c5ea4abe1997eff540e338c
codrelphi <chancerel.codjovi@gmail.com>
2021-12-29 21:43:26 +0100

    Remove unnecessary variables declaration

--- day5_2.js
+++ day5_2.js
@@ -6,8 +6,7 @@
 */
 
 function sides(literals, ...expressions) {
-    let A, P;
-    [A, P] = [expressions[0], expressions[1]];
+    let [A, P] = [expressions[0], expressions[1]];
     let s1 = (P + Math.sqrt(Math.pow(P, 2) - 16 * A)) / 4;
     let s2 = (P - Math.sqrt(Math.pow(P, 2) - 16 * A)) / 4;
     return [s1, s2].sort();
