fb8516a7e3f523e725e5b0cbedf14cfd4f894da4
codrelphi <chancerel.codjovi@gmail.com>
2021-12-29 21:41:16 +0100

    Day 5: Template Literals

--- day5_2.js
+++ day5_2.js
@@ -4,3 +4,11 @@
 	source: https://www.hackerrank.com/challenges/js10-inheritance/problem
 	Day 5: Template Literals
 */
+
+function sides(literals, ...expressions) {
+    let A, P;
+    [A, P] = [expressions[0], expressions[1]];
+    let s1 = (P + Math.sqrt(Math.pow(P, 2) - 16 * A)) / 4;
+    let s2 = (P - Math.sqrt(Math.pow(P, 2) - 16 * A)) / 4;
+    return [s1, s2].sort();
+}
