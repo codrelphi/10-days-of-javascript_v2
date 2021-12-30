f55ae7641b589a624f5ae08ad7403c12cb34a692
codrelphi <chancerel.codjovi@gmail.com>
2021-12-30 22:28:18 +0100

    the code for Day 6: Bitwise Operators

--- day6_1.js
+++ day6_1.js
@@ -4,3 +4,20 @@
 	source: https://www.hackerrank.com/challenges/js10-bitwise/problem
 	Day 6: Bitwise Operators
 */
+
+function getMaxLessThanK(n, k) {
+   let r = [];
+    for (let i=1; i<=n; i++) {
+        for (let j=i+1; j<=n; j++) {
+            if ((i & j) < k) {
+                r.push(i & j);
+            }
+        }
+    }
+    //return Math.max(...r);
+    let max = r[0];
+    for (let el of r) {
+        if (el > max) max = el;
+    }
+    return max;
+}
